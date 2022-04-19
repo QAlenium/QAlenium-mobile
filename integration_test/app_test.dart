69cc67be4c775ae4dbd2d6b33398736fe3971edd
Gabriel Aguido Fraga <gabriel_aguido@hotmail.com>
2022-04-18 21:56:55 -0400

    fixing ci to run integration tests

---
+++ integration_test/app_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'package:qalenium_mobile/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets('Test1', (WidgetTester tester) async {
+          app.main();
+          await tester.pumpAndSettle();
+        });
+  });
+}
